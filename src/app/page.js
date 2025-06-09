794c1b188109955348637298abcce1ac8ee40ed6
JoeyZ56 <jzazzi56@gmail.com>
2025-06-09 14:34:32 -0700

    creating state and a handle function to get info from checkboxes.jsx and store it

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,27 @@
-import PasswordDisplay from "@/components/PasswordDisplay";
+//Parent
+"use client";
+import { useState } from "react";
+import PasswordDisplay from "@/components/PasswordContainer";
 import Checkboxes from "@/components/Checkboxes";
 import Buttons from "@/components/Buttons";
 
 export default function Home() {
+  const [options, setOptions] = useState({
+    includeSymbols: false,
+    includeNumbers: false,
+  }); // state to manage options for checkboxes
+
+  const [password, setPassword] = useState("");
+
+  const handleOptionsChange = (newOptions) => {
+    setOptions(newOptions);
+  }; // handles the changes to checkboxes.jsx. setting the changes to newOptions then storing them in setOptions as the new state
   return (
     <div className="items-center justify-items-center mt-56">
       <main className="flex justify-center flex-col gap-4">
         <PasswordDisplay />
-        <Checkboxes />
+        <Checkboxes onOptionsChange={handleOptionsChange} />{" "}
+        {/**Passing newOptions to checkboxes to grab new data */}
         <Buttons />
       </main>
     </div>
