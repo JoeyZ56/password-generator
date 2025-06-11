25113c39bb53084edea07756517b0add8217d539
JoeyZ56 <jzazzi56@gmail.com>
2025-06-10 22:49:47 -0700

    finsihed passing data to from parent to children and creating handle function for generating a password

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import PasswordDisplay from "@/components/PasswordContainer";
 import Checkboxes from "@/components/Checkboxes";
 import Buttons from "@/components/Buttons";
+import { generatePassword } from "@/utils/generatePassword";
 
 export default function Home() {
   const [options, setOptions] = useState({
@@ -11,18 +12,24 @@ export default function Home() {
     includeNumbers: false,
   }); // state to manage options for checkboxes
 
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState(""); //State to hold password
 
   const handleOptionsChange = (newOptions) => {
     setOptions(newOptions);
-  }; // handles the changes to checkboxes.jsx. setting the changes to newOptions then storing them in setOptions as the new state
+  }; // handles the changes to checkboxes.jsx. setting the changes to newOptions then storing them in setOptions as the new state.
+
+  const handlePGeneratePassword = () => {
+    const newPassword = generatePassword(options); // from utils
+    setPassword(newPassword);
+  }; // generates a password and sets it to the state
+
   return (
     <div className="items-center justify-items-center mt-56">
       <main className="flex justify-center flex-col gap-4">
-        <PasswordDisplay />
+        <PasswordDisplay password={password} />
         <Checkboxes onOptionsChange={handleOptionsChange} />{" "}
         {/**Passing newOptions to checkboxes to grab new data */}
-        <Buttons />
+        <Buttons onGenerate={handlePGeneratePassword} password={password} />
       </main>
     </div>
   );
