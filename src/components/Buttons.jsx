a05c35f4fc130406191f0a37f0ba3ae348ae621c
JoeyZ56 <jzazzi56@gmail.com>
2025-06-10 22:50:57 -0700

    got data from parent and siblings, set onClick methods and created copy passowrd logic

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
-const Buttons = () => {
+const Buttons = ({ onGenerate, password }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopyPassword = () => {
+    if (password) {
+      navigator.clipboard.writeText(password);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000); // reset after 1 second
+    }
+  };
+
   return (
     <div className="items-center justify-items-center space-x-6">
-      <button className="bg-green-400 rounded-sm border-solid border-inherit">
+      <button
+        onClick={onGenerate}
+        className="bg-green-400 rounded-sm border-solid border-inherit cursor-pointer"
+      >
         Generate password
       </button>
-      <button>Copy Password</button>
+      <button onClick={handleCopyPassword} className="cursor-pointer">
+        {copied ? "Password Copied" : "Copy Password"}
+      </button>
     </div>
   );
 };
